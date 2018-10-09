ff2c91c16632edf7592fdf9d1de3427a026fe35c
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-10-08 17:31:13 -0700

    Update and Delete files on command

--- datastore/index.js
+++ datastore/index.js
@@ -55,24 +55,27 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  var filePath = path.join(exports.dataDir, (id + '.txt'));
+  fs.stat(filePath, (err, data) => {
+    if (err) {
+      callback(err, {id: id, text: text});
+    } else {
+      fs.writeFile(filePath, text, (err) => {
+        if (err) {
+          throw ('[ERROR] cannot update file');
+        } else {
+          callback(null, {id: id, text: text});
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var filePath = path.join(exports.dataDir, (id + '.txt'));
+  fs.unlink(filePath, (err) => {
+    callback(err);
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
